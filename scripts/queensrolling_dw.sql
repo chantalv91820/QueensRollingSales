1bdc1274192dd721b862662676d836eece9c5ea6
Chantal Villacres <chantal.villacres@baruchmail.cuny.edu>
2024-04-18 15:36:18 -0400

    Updated DW Script

--- scripts/queensrolling_dw.sql
+++ scripts/queensrolling_dw.sql
@@ -1,58 +1,64 @@
-CREATE SCHEMA IF NOT EXISTS "instance";
+#Script to create DW 
 
-CREATE  TABLE "instance".dim_date ( 
-	date_id              BIGINT  NOT NULL  ,
-	sale_date            DATE    ,
-	month_number         INT    ,
-	CONSTRAINT pk_dim_date PRIMARY KEY ( date_id )
+CREATE SCHEMA INSTANCE;
+
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_date ( 
+	date_id BIGINT NOT NULL  ,
+	sale_date DATE  ,
+	month_number INT  
  );
 
-CREATE  TABLE "instance".dim_location ( 
-	location_id          BIGINT  NOT NULL  ,
-	address              VARCHAR    ,
-	zip_code             INT    ,
-	block                INT    ,
-	lot                  INT    ,
-	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_date ADD PRIMARY KEY ( date_id )  NOT ENFORCED;
+
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_location ( 
+	location_id BIGINT NOT NULL  ,
+	address VARCHAR  ,
+	zip_code INT  ,
+	block INT  ,
+	lot INT  
  );
 
-CREATE UNIQUE INDEX unq_address ON "instance".dim_location ( address );
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_location ADD PRIMARY KEY ( location_id )  NOT ENFORCED;
 
-CREATE  TABLE "instance".dim_neighborhoods ( 
-	neighborhood_id      BIGINT  NOT NULL  ,
-	neighborhood_name    VARCHAR(30)    ,
-	avg_price            DECIMAL(2,2)    ,
-	max_price            DECIMAL(2,2)    ,
-	min_price            DECIMAL(2,2)    ,
-	number_of_sales      INT    ,
-	CONSTRAINT pk_fact_neighborhoods PRIMARY KEY ( neighborhood_id )
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_neighborhoods ( 
+	neighborhood_id BIGINT NOT NULL  ,
+	neighborhood_name VARCHAR(30)  ,
+	avg_price DECIMAL(2,2)  ,
+	max_price DECIMAL(2,2)  ,
+	min_price DECIMAL(2,2)  ,
+	number_of_sales INT  
  );
 
-CREATE  TABLE "instance".dim_property_type ( 
-	property_type_id     BIGINT  NOT NULL  ,
-	building_class       VARCHAR    ,
-	tax_class            VARCHAR    ,
-	CONSTRAINT pk_dim_property_type PRIMARY KEY ( property_type_id )
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_neighborhoods ADD PRIMARY KEY ( neighborhood_id )  NOT ENFORCED;
+
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_property_type ( 
+	property_type_id BIGINT NOT NULL  ,
+	building_class VARCHAR  ,
+	tax_class VARCHAR  
  );
 
-CREATE  TABLE "instance".dim_year_built ( 
-	year_id              INT  NOT NULL  ,
-	"year"               INT    ,
-	CONSTRAINT pk_dim_year_built PRIMARY KEY ( year_id )
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_property_type ADD PRIMARY KEY ( property_type_id )  NOT ENFORCED;
+
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_year_built ( 
+	year_id INT NOT NULL  ,
+	year INT  
  );
 
-CREATE  TABLE "instance".facts_properties ( 
-	fact_id              BIGINT  NOT NULL  ,
-	property_id          INT  NOT NULL  ,
-	sale_price           DECIMAL(2,2)    ,
-	total_units          INT    ,
-	land_sq_feet         INT    ,
-	gross_sq_feet        INT    ,
-	residential_units    INT    ,
-	commercial_units     INT    ,
-	price_per_sqft       DECIMAL(2,2)    ,
-	mean_per_sqft        DECIMAL(2,2)    ,
-	square_differences_mean DECIMAL(2,2)    ,
-	variance_per_sqft    DECIMAL(2,2)    ,
-	CONSTRAINT pk_facts_properties PRIMARY KEY ( fact_id, property_id )
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.dim_year_built ADD PRIMARY KEY ( year_id )  NOT ENFORCED;
+
+CREATE TABLE QueensRollingSales_DW_LGL.INSTANCE.facts_properties ( 
+	fact_id BIGINT NOT NULL  ,
+	property_id INT NOT NULL  ,
+	sale_price DECIMAL(2,2)  ,
+	total_units INT  ,
+	land_sq_feet INT  ,
+	gross_sq_feet INT  ,
+	residential_units INT  ,
+	commercial_units INT  ,
+	price_per_sqft DECIMAL(2,2)  ,
+	mean_per_sqft DECIMAL(2,2)  ,
+	square_differences_mean DECIMAL(2,2)  ,
+	variance_per_sqft DECIMAL(2,2)  
  );
+
+ALTER TABLE QueensRollingSales_DW_LGL.INSTANCE.facts_properties ADD PRIMARY KEY ( fact_id, property_id )  NOT ENFORCED;
